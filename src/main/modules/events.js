99b2697b1986ecc5d65b7de0e3d5c390e2a32b96
jojobyte <184880+jojobyte@users.noreply.github.com>
2018-06-10 12:39:39 -0600

    fix(events): Fix js exception error after clone

--- src/main/modules/events.js
+++ src/main/modules/events.js
@@ -426,13 +426,13 @@ export class Events {
 
   ipcOpenFolder () {
     ipcMain.on('open-folder', (event, opts) => {
-      fs.stat(opts.folder, (err, stat) => {
-        const currentBaseDir = opts.folder.split(path.sep + opts.owner)[0];
+      fs.stat(opts, (err, stat) => {
+        const currentBaseDir = opts.split(path.sep + opts.owner)[0];
         const compareBaseDir = global.baseCloneDir === currentBaseDir;
         if(err == null) {
           // console.warn('folder exists');
-          const opened = shell.openItem(opts.folder);
-          event.sender.send('log', 'open-folder', [opts.folder, opened]);
+          const opened = shell.openItem(opts);
+          event.sender.send('log', 'open-folder', [opts, opened]);
         } else if(!compareBaseDir) {
           const directory = path.join(global.baseCloneDir, opts.owner, opts.name);
           // console.warn('folder does not exist');
@@ -442,9 +442,9 @@ export class Events {
               this.utilsDb.updateById(opts._id, {
                 folder: directory,
               }, 'updatedAt');
-              opts.folder = directory;
-              const opened = shell.openItem(opts.folder);
-              event.sender.send('log', 'open-folder', [opts.folder, opened]);
+              opts = directory;
+              const opened = shell.openItem(opts);
+              event.sender.send('log', 'open-folder', [opts, opened]);
             }
             // console.warn('compare folders', global.baseCloneDir, currentBaseDir, compareBaseDir);
           });
